318af021541adf7752ac19f81b05571f46d2fbd9
Brett Brist <brettbrist@gmail.com>
2023-04-10 00:05:10 -0600

    Initial commit

---
+++ test/aws-cdk8s.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdk8S from '../lib/aws-cdk8s-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk8s-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdk8S.AwsCdk8SStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
